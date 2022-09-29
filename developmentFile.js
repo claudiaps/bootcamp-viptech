d8b8643be73b2bfcc4d2f7f0cca0bd6bfebbedb3
claudiaps <clp.sampedro@gmail.com>
2022-09-28 22:17:53 -0300

    resolve pr

--- developmentFile.js
+++ developmentFile.js
@@ -1,3 +1,3 @@
-const array = [1, 2, 3, 4]
-const numerosPares = array.filter(number => number % 2)
+const numberArray = [1, 2, 3, 4]
+const numerosPares = numberArray.filter(number => number % 2)
 console.log(numerosPares)
